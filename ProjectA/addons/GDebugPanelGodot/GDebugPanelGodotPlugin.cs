ec92e4ab0fb60145965eba0b05dbfdc016c43a76
Guillem Sunyer <gsunyercaldu@gmail.com>
2024-01-21 22:02:14 +0100

    Working on plugin

---
+++ ProjectA/addons/GDebugPanelGodot/GDebugPanelGodotPlugin.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+namespace GDebugPanelGodot
+{
+    [Tool]
+    public partial class GDebugPanelGodotPlugin : EditorPlugin
+    {
+        public override void _EnterTree()
+        {
+            
+        }
+    }   
+}
